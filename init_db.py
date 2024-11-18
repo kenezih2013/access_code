e1a186622507b057f6ee8a9609bb12cae84f30f7
kenezih2013 <77634742+kenezih2013@users.noreply.github.com>
2024-11-18 20:37:18 +0100

    2nd commit

--- init_db.py
+++ init_db.py
@@ -8,13 +8,14 @@ with open('schema.sql') as f:
 cur = connection.cursor()
 
 # Test data
-cur.execute("INSERT INTO posts (guest_name, host_address, access_code) VALUES (?, ?, ?)",
-            ('Jude Samson', '12, King Haggai Rd', 51232)
+
+cur.execute("INSERT INTO admin_users (full_name, a_address, email, a_profile, validity_date) VALUES (?, ?, ?, ?, ?)",
+            ('Kenneth Ezih', '1, CCC Edidi Ave', 'kenezih@gmail.com', 'super admin', '2099-12-31')
             )
 
-cur.execute("INSERT INTO users (full_name, resident_address, email, validity_date) VALUES (?, ?, ?, ?)",
-            ('Taiwo Samson', '5, Nduma Kalu  Rd', 'taiwo.samson@gmail.com', '2024-08-20')
-            )
+#cur.execute("INSERT INTO posts (guest_name, host_address, access_code, user_id) VALUES (?, ?, ?, ?)",
+#            ('Jude Samson', '12, King Haggai Rd', 51232, 1)
+#            )
 
 connection.commit()
 connection.close()
